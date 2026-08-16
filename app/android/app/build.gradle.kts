24730f080bbb3570f7b78a9eff63379d1c48a88f
chenflxs <2665955378@qq.com>
2026-08-16 16:44:07 +0800

    release: Android 包名改为 kikoeta.chenflxs，releases 目录不入库
    - applicationId 由 com.kikoeta.kikoeta_app 改为 kikoeta.chenflxs（对应 build-docs/关于包名.txt）
    - 签名改用 build-docs/kikoeta-key.jks（key.properties 指向，不入库）
    - releases/ 加入 .gitignore

--- app/android/app/build.gradle.kts
+++ app/android/app/build.gradle.kts
@@ -27,8 +27,7 @@ configure<ApplicationExtension> {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.kikoeta.kikoeta_app"
+        applicationId = "kikoeta.chenflxs"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
